8de313e9f7c2c375c9a0e5ff87cf5275d9e6d66b
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2022-09-05 07:27:20 -0300

    feat: finished test by mocking database

--- src/controllers/admin.controler.spec.ts
+++ src/controllers/admin.controler.spec.ts
@@ -1,10 +1,17 @@
 import supertest from "supertest";
 import { getApp } from "../getApp";
+import { jobsService } from "../services/jobs.service";
 
 const app = getApp();
 
+jest.mock("../services/jobs.service", () => ({
+  jobsService: {
+    findBestClientsInRange: jest.fn(),
+  },
+}));
+
 describe("admin.controller", () => {
-  describe("bestClients", () => {
+  describe("findBestClientsInRange", () => {
     it("returns 400 with wrong startDate", async () => {
       await supertest(app)
         .get("/admin/best-clients")
@@ -17,7 +24,7 @@ describe("admin.controller", () => {
         .query({ start: "wrong", end: new Date() })
         .expect(400);
     });
-    it("returns Programmer as best clients", async () => {
+    it("returns 200 with correct params", async () => {
       const response = await supertest(app)
         .get("/admin/best-clients")
         .query({
@@ -25,32 +32,6 @@ describe("admin.controller", () => {
           end: "2220-08-15T19:11:26.737Z",
         })
         .expect(200);
-      expect(response.body).toMatch(/Programmer/);
     });
   });
-
-  // describe("bestPro", () => {
-  //   it("returns 400 with wrong startDate", async () => {
-  //     await supertest(app)
-  //       .get("/admin/best-clients")
-  //       .query({ start: "wrong", end: new Date() })
-  //       .expect(400);
-  //   });
-  //   it("returns 400 with wrong endDate", async () => {
-  //     await supertest(app)
-  //       .get("/admin/best-clients")
-  //       .query({ start: "wrong", end: new Date() })
-  //       .expect(400);
-  //   });
-  //   it("returns Programmer as best clients", async () => {
-  //     const response = await supertest(app)
-  //       .get("/admin/best-clients")
-  //       .query({
-  //         start: "2002-08-10T19:11:26.737Z",
-  //         end: "2220-08-15T19:11:26.737Z",
-  //       })
-  //       .expect(200);
-  //     expect(response.body).toMatch(/Programmer/);
-  //   });
-  // });
 });
